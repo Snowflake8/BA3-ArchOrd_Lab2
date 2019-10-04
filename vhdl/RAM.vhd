85ce75f6ff71b02121a46cae56077d06990a17b6
Nicolas Raulin <nicolas.raulin@epfl.ch>
2019-10-04 18:22:32 +0200

    RAM + decoder, need to test with testbench

--- vhdl/RAM.vhd
+++ vhdl/RAM.vhd
@@ -14,5 +14,57 @@ entity RAM is
 end RAM;
 
 architecture synth of RAM is
+
+	TYPE reg_type is array(0 to 1023) of std_logic_vector(31 downto 0);
+
+	SIGNAL s_read : std_logic_vector(31 downto 0);
+	SIGNAL s_address : std_logic_vector(9 downto 0);
+	SIGNAL s_enable : std_logic;
+
+	SIGNAL s_integer_address : integer;
+
+
 begin
+
+	s_integer_address <= to_integer(unsigned(address));
+
+	s_read <= reg_type(s_integer_address);
+
+
+	dff : process(clk) is
+	begin
+
+		if rising_edge(clk) then
+			s_address <= address;
+			s_enable <= (read and cs);
+		end if;
+
+	end process dff;
+
+
+
+	triStateBuffer : process(clk) is
+	begin
+
+		if rising_edge(clk) then
+			rddata <= s_read when (s_enable = '1') else (others=>'Z');
+		end if;
+
+	end process triStateBuffer;
+
+
+
+	writeRAM : process(clk) is
+	begin
+
+		if rising_edge(clk) then
+			if (write = '1' and cs = '1') then
+				reg_type(s_integer_address) <= wrdata;
+
+			end if;
+		end if;
+
+	end process readRAM;
+
+
 end synth;
--- vhdl/decoder.vhd
+++ vhdl/decoder.vhd
@@ -12,4 +12,22 @@ end decoder;
 
 architecture synth of decoder is
 begin
+
+	decode : process(address) is
+	begin
+
+		cs_LEDS <= '0';
+		cs_RAM <= '0';
+		cs_ROM <= '0';
+
+		if (address >= x"0000" and address <= x"0FFC") then 
+			cs_ROM <= '1';
+		elsif (address >= x"1000" and address <= x"1FFC") then 
+			cs_ROM <= '1';
+		elsif (address >= x"2000" and address <= x"200C") then
+			cs_LEDS <= '1';
+		end if;
+
+	end process decode;
+
 end synth;
